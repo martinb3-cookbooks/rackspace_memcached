4a027190f9fffab779ff4dda3a505169fa8d0c70
Martin Smith <martin@mbs3.org>
2014-01-28 15:21:16 -0600

    bundler and versions and gem deps now correct to use strainer

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 default['memcached']['config']['port'] = '11211'
-pcmemory = node['memory']['total'] * 0.75 / 1000
-default['memcached']['config']['memory'] = pcmemory.to_i
+default['memcached']['config']['memory'] =
+  (node['memory']['total'].to_i * 0.75 / 1000).to_i
 default['memcached']['config']['maxconnections'] = '1024'
 default['memcached']['config']['memcachedconf'] = {}
